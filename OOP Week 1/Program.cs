488d9ef399f7b49a228bf333a0266c45f4919348
James Relph <J.RELPH-2018@hull.ac.uk>
2021-02-04 12:26:41 +0000

    Add project files.

---
+++ OOP Week 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOP_Week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            int currentIndex = 0;
+
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("Please enter an integer.");
+                string line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    repeat = false;
+                }
+
+                else
+                {
+                    try
+                    {
+                        int number = int.Parse(line);
+                        Console.WriteLine("You have entered the integer " + number);
+                        numbers[currentIndex] = number;
+                        currentIndex++;
+                        if( currentIndex == numbers.Length)
+                        {
+                            int expandedNumberOfCells = currentIndex + 10;
+                            int[] expandedNumbers = new int[expandedNumberOfCells];
+                            for (int i = 0; i < currentIndex; i++)
+                            {
+                                expandedNumbers[i] = numbers[i];
+                            }
+                            numbers = expandedNumbers;
+                        }
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine("That's not an integer.");
+                    }
+                }
+            }
+            while (repeat);
+            for (int i = 0; i < currentIndex; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
+}

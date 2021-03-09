76c6e53be49a2030d4f3cd9695ab1c0756342207
James Relph <J.RELPH-2018@hull.ac.uk>
2021-03-09 12:04:14 +0000

    Added Mothods

--- OOP Lab 2/ResizableIntArray.cs
+++ OOP Lab 2/ResizableIntArray.cs
@@ -6,6 +6,44 @@ namespace OOP_Lab_2
 {
     class ResizableIntArray
     {
+        int[] mStorage = new int[10];
+        int mCurrentIndex = 0;
 
+        public void Add(int pNumber)
+        {
+            mStorage[mCurrentIndex] = pNumber;
+            mCurrentIndex++;
+            if (IsFull())
+            {
+                ExpandStorage(10);
+            }
+        }
+
+        public void ExpandStorage(int pSizeIncrease)
+        {
+            int[] increasedStorage = new int[mCurrentIndex + pSizeIncrease];
+            for (int i = 0; i< mCurrentIndex; i++)
+            {
+                increasedStorage[i] = mStorage[i];
+            }
+            mStorage = increasedStorage;
+        }
+
+        public bool IsFull()
+        {
+            if (mCurrentIndex < mStorage.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void WriteContentsToConsole()
+        { 
+            for (int i = 0; i < mCurrentIndex; i++)
+            {
+                Console.WriteLine(mStorage[i]);
+            }
+        }
     }
 }

f6e906f06d6cc461d7804c11dfaddfbf7acb1c79
James Relph <J.RELPH-2018@hull.ac.uk>
2021-03-13 13:31:51 +0000

    Added constructor to highscores with name and score variables

---
+++ Lab 3/Highscores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3
+{
+    class HighScores
+    {
+        private string mName;
+        private int mScore;
+
+        public string Name
+        {
+            get { return mName;  }
+        }
+
+        public int Score
+        {
+            get { return mScore; }
+        }
+
+        public HighScores(string pName, int pScore)
+        {
+            mName = pName;
+            mScore = pScore;
+        }
+        
+    }
+}
